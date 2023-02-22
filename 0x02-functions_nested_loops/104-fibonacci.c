1d9f9b2a2332a1a7c85d727b66e7150da9d95f6b
LaudRam <LaudiciaRamasenya@gmail.com>
2023-02-21 18:21:11 -0800

    Final Task 16 commit/ update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,16 +6,30 @@
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int a = 1;
-	unsigned long int b = 2;
+	unsigned long int i, a = 1, b = 2, a1, a2, b1, b2;
 
 	printf("%lu", a);
-	for (i = 1; i < 85; i++)
+
+	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", b);
 		b += a;
 		a = b - a;
+	}
+
+	a1 = (a / 1000000000);
+	a2 = (a % 1000000000);
+	b1 = (b / 1000000000);
+	b2 = (b % 1000000000);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", b1 + (b2 / 1000000000));
+		printf("%lu", b2 % 1000000000);
+		b1 = b1 + a1;
+		a1 = b1 - a1;
+		b2 = b2 + a2;
+		a2 = b2 - a2;
 	}
 
 	printf("\n");
