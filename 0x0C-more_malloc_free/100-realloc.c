bc090937fc0d25b4d23bcba9cc07caf91405a04e
LaudRam <laudiciaramasenya@gmail.com>
2023-03-16 22:43:47 +0000

    fixed betty warning

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,9 +17,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	if (new_size == 0 && ptr)
+	{
 		free(ptr);
 		return (NULL);
-
+	}
 	if (!ptr)
 		return (malloc(new_size));
 
