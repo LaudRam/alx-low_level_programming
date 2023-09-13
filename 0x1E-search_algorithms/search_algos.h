7916efbbae7b246429b54246a7fb8d7397b59ba8
LaudRam <laudiciaramasenya@gmail.com>
2023-09-13 19:54:31 +0200

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,6 +12,7 @@ void print_array(int *array, size_t low, size_t high);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int b_search(int *array, size_t size, int low, int high, int value);
 
 
 #endif
